755a32af7d53340525c3ecd219fdf04eebad2346
Gamilliam <brnlcecile@gmail.com>
2024-02-22 16:55:26 +0100

    Minor visual changes : order in mock data and a css color

--- rbl_frontend/src/components/hub/hub_hub_informations.tsx
+++ rbl_frontend/src/components/hub/hub_hub_informations.tsx
@@ -21,24 +21,25 @@ export const gamesInformations = [
     nameGame: "Lootery",
     routeGame: "tgpq",
     descriptionGame:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at tellus vestibulum, efficitur lorem in, mattis tellus. Nullam vestibulum sem sit amet nunc faucibus, id viverra nibh pharetra. Phasellus mollis dapibus dolor, blandit scelerisque leo porttitor vel. Nam molestie odio ut dui ornare, eu suscipit nunc TINCIDUNT.",
+      "Sometimes, you may win. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at tellus vestibulum, efficitur lorem in, mattis tellus. Nullam vestibulum sem sit amet nunc faucibus, id viverra nibh pharetra. Phasellus mollis dapibus dolor, blandit scelerisque leo porttitor vel. Nam molestie odio ut dui ornare, eu suscipit nunc TINCIDUNT.",
   },
   {
     imageGame: LogoRBL,
-    nameGame: "Lootery",
+    nameGame: "The dog show",
     routeGame: "tgpq",
-    descriptionGame: "LOREM",
+    descriptionGame:
+      "Curabitur at tellus vestibulum, efficitur lorem in, mattis tellus. LOREM",
   },
   {
     imageGame: LogoRBL,
-    nameGame: "Mount",
+    nameGame: "Mount and grass",
     routeGame: "tgpq",
     descriptionGame:
       "Etiam consequat erat diam, sed bibendum ex ornare id. Nulla non maximus lectus, vel mollis nisl. Suspendisse consequat urna ac dolor porta, luctus condimentum enim tempus. Aenean convallis turpis vitae mi posuere, vel teempor odio semper. Nulla mattis enim auctor nisl imperdiet viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec lacus mauris, posuere id fermentum ut, ultricies at libero. Donec lorem velit, bibendum at lacinia in, facilisis id ELIT.",
   },
   {
     imageGame: LogoRBL,
-    nameGame: "Patata",
+    nameGame: "Patata Banana",
     routeGame: "tgpq",
     descriptionGame:
       "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vivamus tincidunt ornare arcu, at pulvinar nisl. In eget est id lectus rhoncus efficitur ac ut lorem. Phasellus egestas hendrerit urna ut CONSEQUAT.",
@@ -70,19 +71,19 @@ export const allGamesStatsForUser = [
     ],
   },
   {
-    game: "Mount-'_\"^¨$*µ £&é\"'(-è_     çà=+´~#{[|`^@]}",
-    nbWin: 1,
-    nbLose: 1,
-    gameStats: [],
-  },
-  {
-    game: "Poulet vengeur",
+    game: "The dog show",
     nbWin: 22,
     nbLose: 9,
     gameStats: [
       { name: "Camion", value: 22222 },
       { name: "Poeme", value: 62 },
     ],
+  },
+  {
+    game: "Mount and grass-'_\"^¨$*µ £&é\"'(-è_     çà=+´~#{[|`^@]}",
+    nbWin: 1,
+    nbLose: 1,
+    gameStats: [],
   },
   {
     game: "Patata Banana",
